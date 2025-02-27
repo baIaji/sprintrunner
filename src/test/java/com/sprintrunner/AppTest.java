d7e8ab4e8d423f22caca86b80b6f54bfa036b5b4
Balaji <balaji.cuttackam@gmail.com>
2025-02-27 13:14:51 +0000

    STRY0010038: Test commit 1

--- src/test/java/com/sprintrunner/AppTest.java
+++ src/test/java/com/sprintrunner/AppTest.java
@@ -17,6 +17,7 @@ class AppTest {
         assertEquals(1, 1);
         assertEquals(true, true);
         assertEquals(true, true);
+        assertEquals(true, true);
         assertTrue(true);
     }
 
